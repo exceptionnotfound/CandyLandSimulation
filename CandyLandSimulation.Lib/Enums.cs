0fe25831616a6be3f27fadd80b98c25fc12e3e9e
exceptionnotfound <mpjones428@gmail.com>
2015-07-08 07:00:38 -0700

    Added Enums for CandyColor

---
+++ CandyLandSimulation.Lib/Enums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandyLandSimulation.Lib
+{
+    public enum CandyColor
+    {
+        Red,
+        Orange,
+        Yellow,
+        Green,
+        Blue,
+        Purple,
+        Star,
+        IceCream,
+        Gingerbread,
+        Chocolate,
+        Lollipop,
+        IcePop,
+        Rainbow
+    }
+}
